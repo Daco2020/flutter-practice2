7e37c395528656f05520b5c3cd28c0c40e39b28e
Daco2020 <dacokim32@gmail.com>
2024-03-15 13:32:18 +0900

    feat: Expanded 위젯 구현

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -18,7 +18,7 @@ class _MainScreenState extends State<MainScreen> {
           // Column 위젯을 사용하여 세로로 위젯을 배치
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
+          children: [
             Text('반갑습니다', style: TextStyle(fontSize: 30)),
             Text('여기는 메인화면입니다'),
             Text('다 덤벼주세요'),
@@ -28,6 +28,15 @@ class _MainScreenState extends State<MainScreen> {
               children: [
                 Text('누구부터'),
                 Text('혼내줄까?'),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(flex: 2, child: Text("변")),
+                Expanded(flex: 1, child: Text("덕")),
+                Expanded(flex: 1, child: Text("순")),
+                // Expanded 위젯을 사용하여 가로 공간을 꽉 채우기
+                // flex 속성을 사용하여 공간 비율을 지정
               ],
             )
           ],
