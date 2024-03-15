8b89cd20d38a71a03e43288dd8784b8cc72c759a
Daco2020 <dacokim32@gmail.com>
2024-03-15 13:52:00 +0900

    feat: SizedBox 위젯 구현

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -19,10 +19,10 @@ class _MainScreenState extends State<MainScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('반갑습니다', style: TextStyle(fontSize: 30)),
-            Text('여기는 메인화면입니다'),
-            Text('다 덤벼주세요'),
-            Row(
+            const Text('반갑습니다', style: TextStyle(fontSize: 30)),
+            const Text('여기는 메인화면입니다'),
+            const Text('다 덤벼주세요'),
+            const Row(
               // Row 위젯을 사용하여 가로로 위젯을 배치
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -30,7 +30,7 @@ class _MainScreenState extends State<MainScreen> {
                 Text('혼내줄까?'),
               ],
             ),
-            Row(
+            const Row(
               children: [
                 Expanded(flex: 2, child: Text("변")),
                 Expanded(flex: 1, child: Text("덕")),
@@ -40,14 +40,25 @@ class _MainScreenState extends State<MainScreen> {
               ],
             ),
             Container(
-              // Container 위젯을 사용하여 내부 공간을 구성
-              // 위젯은 사이즈를 지정할 수 없기 때문에 Container 위젯을 사용하여 사이즈를 지정
-              width: 300,
+                // Container 위젯을 사용하여 내부 공간을 구성
+                // 위젯은 사이즈를 지정할 수 없기 때문에 Container 위젯을 사용하여 사이즈를 지정
+                width: 300,
+                height: 100,
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.only(left: 20),
+                alignment: Alignment.center,
+                // color: Colors.amberAccent,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.amberAccent),
+                child: const Text('컨테이너 안에 텍스트')),
+            const SizedBox(
+              // SizedBox 위젯을 사용하여 사이즈를 지정
+              // 특정 공간이 필요할 때 주로 사용
+              width: 100,
               height: 100,
-              alignment: Alignment.center,
-              child: Text('컨테이너 안에 텍스트'),
-              color: Colors.amberAccent,
-            ),
+              child: Text("SizedBox 위젯을 사용하여 사이즈를 지정"),
+            )
           ],
         ),
       ),
