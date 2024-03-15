dfed122fe7ca4cd485d8472c43bfe38f06c1fcea
Daco2020 <dacokim32@gmail.com>
2024-03-15 13:22:33 +0900

    feat: Column 위젯 구현

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -11,17 +11,17 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Main Screen'),
-      ),
+      appBar: AppBar(title: const Text('메인 화면')),
       body: const Center(
+        // Center 위젯을 사용하여 화면 중앙에 텍스트를 배치
         child: Column(
+          // Column 위젯을 사용하여 세로로 위젯을 배치
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'Main Screen',
-              style: TextStyle(fontSize: 30),
-            ),
+            Text('반갑습니다', style: TextStyle(fontSize: 30)),
+            Text('여기는 메인화면입니다'),
+            Text('다 덤벼주세요'),
           ],
         ),
       ),
