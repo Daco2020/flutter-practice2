1233ff1ac350ef886303e5bb093d84570c0c1736
Daco2020 <dacokim32@gmail.com>
2024-03-16 20:54:07 +0900

    feat: TextEditingController TextField 위젯 구현

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -9,21 +9,35 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   List listHello = ["득현", "덕순", "효빈"];
+  TextEditingController idController = TextEditingController();
+  // 유저가 인풋창에 입력한 값을 가져오기 위한 컨트롤러
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('메인 화면')),
-      body: ListView.builder(
-        // 리스트 목록 UI 생성, itemCount에 지정된 숫자만큼 반복한다.
-        itemCount: listHello.length,
-        itemBuilder: (BuildContext context, int index) {
-          var count = index + 1;
-          return ListTile(
-            title: Text('${listHello[index]}'),
-            subtitle: Text('서브 제목 $count'),
-          );
-        },
-      ),
-    );
+        appBar: AppBar(title: const Text('메인 화면')),
+        body: Column(
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: '이름을 입력하세요', // placeholder 역할
+              ),
+            ),
+            TextField(
+              controller: idController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: '아이디를 입력하세요', // placeholder 역할
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print(idController.text.toString());
+              },
+              child: const Text('아이디 가져오기'),
+            ),
+          ],
+        ));
   }
 }
